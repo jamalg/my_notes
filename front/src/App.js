fcf20fd3372111dddc9fe4c019820cadd48704cb
Jamal Gourinda <jgourinda@Jamals-MacBook-Pro.local>
2019-03-31 16:49:02 +0100

    core: add base react app

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{margin: "auto"}}>
+        Assalamou Alaykoum !
+      </div>
+    );
+  }
+}
+
+export default App;
