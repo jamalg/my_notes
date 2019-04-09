2b6214b1c63c81f763fdf05f3d6d14bad1d40eff
Jamal Gourinda <jamal.gourinda.pro@gmail.com>
2019-04-09 12:11:50 +0100

    feat: add redux flow for toc generation and note body cleaning

--- front/src/redux/actions/sync.js
+++ front/src/redux/actions/sync.js
@@ -1,7 +1,7 @@
 import * as defs from '../../defs'
 import * as utils from './utils'
 
-// --> CATEGORIES
+// --> ALL CATEGORIES
 export const allCategoriesFetchRequested = utils.makeCreators(defs.ALL_CATEGORIES_FETCH_REQUESTED)
 export const allCategoriesFetchSuccess = utils.makeCreators(defs.ALL_CATEGORIES_FETCH_SUCCESS)
 export const allCategoriesFetchFailed = utils.makeCreators(defs.ALL_CATEGORIES_FETCH_FAILED, "error")
@@ -11,11 +11,18 @@ export const folderFetchRequested = utils.makeCreators(defs.FOLDER_FETCH_REQUEST
 export const folderFetchSuccess = utils.makeCreators(defs.FOLDER_FETCH_SUCCESS, "folderId")
 export const folderFetchFailed = utils.makeCreators(defs.FOLDER_FETCH_FAILED, "folderId", "error")
 
+export const generateFolderTOCS = utils.makeCreators(defs.GENERATE_FOLDER_TOCS, "folderId")
+
 // --> NOTES
 export const noteFetchRequested = utils.makeCreators(defs.NOTE_FETCH_REQUESTED, "noteId")
 export const noteFetchSuccess = utils.makeCreators(defs.NOTE_FETCH_SUCCESS, "noteId")
 export const noteFetchFailed = utils.makeCreators(defs.NOTE_FETCH_FAILED, "noteId", "error")
+
 export const removeNoteBody = utils.makeCreators(defs.REMOVE_NOTE_BODY, "noteId")
+
+export const generateNoteTOCRequested = utils.makeCreators(defs.GENERATE_NOTE_TOC_REQUESTED, "noteId")
+export const generateNoteTOCSuccess = utils.makeCreators(defs.GENERATE_NOTE_TOC_SUCCESS, "noteId", "toc")
+export const generateNoteTOCFailed = utils.makeCreators(defs.GENERATE_NOTE_TOC_FAILED, "noteId", "error")
 
 // -> GENERIC ADD ENTITIES
 export const addEntities = utils.makeCreators(defs.ADD_ENTITIES, "entities")
--- front/src/redux/reducers/notes.js
+++ front/src/redux/reducers/notes.js
@@ -12,8 +12,23 @@ export default function notes(state=fromJS({}), action) {
                 s.setIn([action.payload.noteId.toString(), "frontStatus"], defs.STATUS.FAILED)
                 .setIn([action.payload.noteId.toString(), "error"], action.payload.error)
             )
+
         case defs.REMOVE_NOTE_BODY:
             return state.deleteIn([action.payload.noteId.toString(), "body"])
+
+        case defs.GENERATE_NOTE_TOC_REQUESTED:
+            return state.setIn([action.payload.noteId.toString(), "tocStatus"], defs.STATUS.REQUESTING)
+        case defs.GENERATE_NOTE_TOC_SUCCESS:
+            return state.withMutations(s =>
+                s.setIn([action.payload.noteId.toString(), "tocStatus"], defs.STATUS.SUCCESS)
+                .setIn([action.payload.noteId.toString(), "toc"], action.payload.toc)
+            )
+        case defs.GENERATE_NOTE_TOC_FAILED:
+            return state.withMutations(s =>
+                s.setIn([action.payload.noteId.toString(), "tocStatus"], defs.STATUS.FAILED)
+                .setIn([action.payload.noteId.toString(), "error"], action.payload.error)
+            )
+
         case defs.ADD_ENTITIES:
             return state.mergeWith((o,n) => o.merge(n) ,fromJS(action.payload.entities.notes))
         default:
